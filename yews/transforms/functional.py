a78924a53db71b719d8feb103a5e08c49b511dc3
Lijun Zhu <gatechzhu@gmail.com>
2019-04-08 21:36:29 -0400

    add basic transforms

---
+++ yews/transforms/functional.py
@@ -0,0 +1,22 @@
+import numpy as np
+import torch
+
+def _is_numpy_waveform(wav):
+    return isinstance(wav, np.ndarray) and (wav.ndim in {1, 2})
+
+def to_tensor(wav):
+    """Convert a ``numpy.ndarray`` to tensor.
+    See ``ToTensor`` for more details.
+    Args:
+        wav (numpy.ndarray): Waveform to be converted to tensor.
+    Returns:
+        Tensor: Converted array.
+    """
+
+    if not(_is_numpy_waveform(wav)):
+        raise TypeError(f"wav should be ndarray. Got {type(wav)}")
+
+    if wav.ndim == 1:
+        wav = wav[None, :]
+
+    return torch.from_numpy(wav).float()
