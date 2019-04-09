fb001db9a0a6fdb617b3f3a7c7622f190f33c5d7
Lijun Zhu <gatechzhu@gmail.com>
2019-04-08 21:34:40 -0400

    add dataset classes

---
+++ yews/__about__.py
@@ -0,0 +1,9 @@
+__title__ = "yews"
+__summary__ = "Your Earthquake Waveform Solution"
+
+__version__ = "0.0.1"
+
+__author__ = "Lijun Zhu"
+__email__ = "gatechzhu@gmail.com"
+
+__license__ = "MIT"
---
+++ yews/__init__.py
@@ -0,0 +1,12 @@
+from yews.__about__ import (
+    __title__,
+    __version__,
+    __summary__,
+)
+
+from yews import dataset
+from yews import catalog
+from yews import transforms
+from yews import train
+from yews import utils
+from yews import models
