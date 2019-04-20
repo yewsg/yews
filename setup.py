1a6235a6918a7c99fa0492ffda9207b44efd7edc
Lijun Zhu <gatechzhu@gmail.com>
2019-04-20 00:49:16 -0400

    Move metadata from setup.py to setup.cfg

--- setup.py
+++ setup.py
@@ -1,12 +1,5 @@
 from setuptools import setup, find_packages
 
-about = {}
-with open("yews/__about__.py") as fp:
-    exec(fp.read(), about)
-
-with open("README.rst", encoding='utf-8') as f:
-    long_description = f.read()
-
 requirements = [
     'numpy',
     'scipy',
@@ -15,24 +8,4 @@ requirements = [
 
 if __name__ == '__main__':
 
-    setup(
-        python_requires='>=3.6',  # Your supported Python ranges
-
-        # metadata
-        name=about['__title__'],
-        version=about['__version__'],
-        author=about['__author__'],
-        author_email=about['__email__'],
-        description="Deep learning toolbox for seismic waveform processing.",
-        license=about['__license__'],
-
-        # README
-        long_description=long_description,
-        long_description_content_type='text/x-rst',
-
-        # package info
-        packages=find_packages(exclude=('tests', 'examples')),
-
-        zip_safe=True,
-        install_requires=requirements,
-    )
+    setup()
