1b15519e4733088166e897f12fec4e806715af29
Lijun Zhu <gatechzhu@gmail.com>
2019-04-21 20:26:30 -0400

    add `yews.datasets.utils` with tests covered 100%

---
+++ tests/test_datasets_utils.py
@@ -0,0 +1,47 @@
+import os
+from pathlib import Path
+
+import pytest
+
+from yews.datasets import utils
+
+class TestBz2Utils():
+
+    def test_bz2_extraction(self):
+        utils.extract_bz2('tests/assets/test.tar.bz2')
+        with Path('test.txt').open('r') as f:
+            assert f.readlines() == ['test\n']
+        os.remove('test.txt')
+
+
+
+class TestUrlUtils():
+
+    def test_sizeof_fmt(self):
+        assert utils.sizeof_fmt(10, 'm') == '10.0m'
+        assert utils.sizeof_fmt(1e1) == '10.0B'
+        assert utils.sizeof_fmt(1e4) == '9.8KB'
+        assert utils.sizeof_fmt(1e7) == '9.5MB'
+        assert utils.sizeof_fmt(1e10) == '9.3GB'
+        assert utils.sizeof_fmt(1e13) == '9.1TB'
+        assert utils.sizeof_fmt(1e16) == '8.9PB'
+        assert utils.sizeof_fmt(1e19) == '8.7EB'
+        assert utils.sizeof_fmt(1e22) == '8.5ZB'
+        assert utils.sizeof_fmt(1e25) == '8.3YB'
+
+    def test_URL(self):
+        # good url
+        url = utils.URL('https://www.dropbox.com/s/qxf6ki0eruv66w5/test.txt?dl=1')
+        # __repr__
+        print(url)
+        # download w/o filename
+        url.download('.')
+        with Path('test.txt').open('r') as f:
+            assert f.readlines() == ['test\n']
+        os.remove('./test.txt')
+        # download w/ filename
+        url.download('.', 'new_test.txt')
+        os.remove('./new_test.txt')
+        # bad url
+        with pytest.raises(ValueError):
+            url = utils.URL('https://www.yews.info/BAD_PAGE.html')
--- yews/datasets/__init__.py
+++ yews/datasets/__init__.py
@@ -1,6 +1,12 @@
-from .base import BaseDataset, PathDataset, is_dataset
-from .files import FileDataset, DatasetArray
-from .dirs import DirDataset, DatasetFolder, DatasetArrayFolder
+from .base import BaseDataset
+from .base import is_dataset
+from .base import PathDataset
+from .dirs import DatasetArrayFolder
+from .dirs import DatasetFolder
+from .dirs import DirDataset
+from .files import DatasetArray
+from .files import FileDataset
+from .utils import *
 
 __all__ = (
     'BaseDataset',
@@ -11,4 +17,3 @@ __all__ = (
     'DatasetFolder',
     'DatasetArrayFolder',
 )
-
