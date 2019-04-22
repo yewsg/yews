0f556077bea0d70592cbab0d3b7e35e75c4bf1c6
Lijun Zhu <gatechzhu@gmail.com>
2019-04-21 20:31:32 -0400

    remove redundant __about__.py

--- yews/__about__.py
+++
@@ -1,9 +0,0 @@
-__title__ = "yews"
-__summary__ = "Your Earthquake Waveform Solution"
-
-__version__ = "0.0.3"
-
-__author__ = "Lijun Zhu"
-__email__ = "gatechzhu@gmail.com"
-
-__license__ = "Apache 2.0"
