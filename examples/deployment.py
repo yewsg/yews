5e38394372a1514d5df16395ead87753834d7cd7
Lijun Zhu <gatechzhu@gmail.com>
2019-06-22 13:07:49 -0400

    update example for sp deployment example

--- examples/deployment.py
+++ examples/deployment.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import obspy
 import torch
@@ -9,9 +10,10 @@ from yews.cpic import pick
 from yews.datasets.utils import stream2array
 from yews.models import cpic
 
+origin_time = obspy.UTCDateTime('2019-05-14T12:58:26.000000Z')
 st = obspy.read('/data/sp.mseed',
-                starttime=obspy.UTCDateTime('2019-05-14T12:58:26.000000Z'),
-                endtime=obspy.UTCDateTime('2019-05-14T16:08:00.000000Z'))
+                starttime=origin_time,
+                endtime=origin_time + 100)
 array = stream2array(st)
 model = cpic(pretrained=True)
 model = nn.DataParallel(model)
@@ -22,18 +24,127 @@ transform = tf.Compose([
     tf.ToTensor(),
 ])
 
-#detect_results = detect(array, 100, 20, model, transform, 2, threshold=0.5)
+fig, axes = plt.subplots(4, 1, figsize=(6, 8), sharex=True)
+
 pick_results = pick(array, 100, 20, model, transform, 0.5)
-#
-import matplotlib.pyplot as plt
-fig, axes = plt.subplots(2, 1)
 ts = st[0].times()
-axes[0].plot(ts, array[2], 'k')
-ax2 = axes[0].twinx()
-#td = np.linspace(5, ts[-1]-15, len(detect_results['detect_p']))
-#ax2.plot(td, detect_results['detect_p'])
-#ax2.plot(td, detect_results['detect_s'])
+ax1 = axes[0]
+ax1.plot(ts, array[2], 'k')
+ax1.set_ylabel('Amplitude (counts)')
+ax2 = ax1.twinx()
 tp = np.linspace(5, ts[-1]-15, len(pick_results['cf_p']))
 ax2.plot(tp, pick_results['cf_p'])
 ax2.plot(tp, pick_results['cf_s'])
+ax2.set_xlim(0, 100)
+ax2.grid()
+ax2.set_ylabel('Probability ratio (log10)')
+
+st = obspy.read('/data/sp.mseed',
+                starttime=origin_time + 1000,
+                endtime=origin_time + 1100)
+array = stream2array(st)
+model = cpic(pretrained=True)
+model = nn.DataParallel(model)
+
+transform = tf.Compose([
+    tf.ZeroMean(),
+    tf.SoftClip(1e-6),
+    tf.ToTensor(),
+])
+
+pick_results = pick(array, 100, 20, model, transform, 0.5)
+ax1 = axes[1]
+ax1.plot(ts, array[2], 'k')
+ax1.set_ylabel('Amplitude (counts)')
+ax2 = ax1.twinx()
+tp = np.linspace(5, ts[-1]-15, len(pick_results['cf_p']))
+ax2.plot(tp, pick_results['cf_p'])
+ax2.plot(tp, pick_results['cf_s'])
+ax2.set_xlim(0, 100)
+ax2.grid()
+ax2.set_ylabel('Probability ratio (log10)')
+
+st = obspy.read('/data/sp.mseed',
+                starttime=origin_time + 2000,
+                endtime=origin_time + 2100)
+array = stream2array(st)
+model = cpic(pretrained=True)
+model = nn.DataParallel(model)
+
+transform = tf.Compose([
+    tf.ZeroMean(),
+    tf.SoftClip(1e-6),
+    tf.ToTensor(),
+])
+
+pick_results = pick(array, 100, 20, model, transform, 0.5)
+ax1 = axes[2]
+ax1.plot(ts, array[2], 'k')
+ax1.set_ylabel('Amplitude (counts)')
+ax2 = ax1.twinx()
+tp = np.linspace(5, ts[-1]-15, len(pick_results['cf_p']))
+ax2.plot(tp, pick_results['cf_p'])
+ax2.plot(tp, pick_results['cf_s'])
+ax2.set_xlim(0, 100)
+ax2.grid()
+ax2.set_ylabel('Probability ratio (log10)')
+
+st = obspy.read('/data/sp.mseed',
+                starttime=origin_time + 3000,
+                endtime=origin_time + 3100)
+array = stream2array(st)
+model = cpic(pretrained=True)
+model = nn.DataParallel(model)
+
+transform = tf.Compose([
+    tf.ZeroMean(),
+    tf.SoftClip(1e-4),
+    tf.ToTensor(),
+])
+
+pick_results = pick(array, 100, 20, model, transform, 0.5)
+ax1 = axes[3]
+ax1.plot(ts, array[2], 'k')
+ax1.set_ylim(-100000, 100000)
+ax1.set_ylabel('Amplitude (counts)')
+ax2 = ax1.twinx()
+tp = np.linspace(5, ts[-1]-15, len(pick_results['cf_p']))
+ax2.plot(tp, pick_results['cf_p'])
+ax2.plot(tp, pick_results['cf_s'])
+ax2.set_xlim(0, 100)
+ax2.grid()
+ax2.set_ylabel('Probability ratio (log10)')
+
+ax1.set_xlabel('Time (second)')
+fig.tight_layout()
+
+fig, ax = plt.subplots(1,1, figsize=(10, 4))
+st = obspy.read('/data/sp.mseed',
+                starttime=origin_time,
+                endtime=origin_time + 7200)
+ts = st[0].times()
+array = stream2array(st)
+model = cpic(pretrained=True)
+model = nn.DataParallel(model)
+
+transform = tf.Compose([
+    tf.ZeroMean(),
+    tf.SoftClip(1e-4),
+    tf.ToTensor(),
+])
+
+pick_results = pick(array, 100, 20, model, transform, 0.5)
+ax1 = ax
+ax1.plot(ts, array[2], 'k')
+ax1.set_ylabel('Amplitude (counts)')
+ax2 = ax1.twinx()
+tp = np.linspace(5, ts[-1]-15, len(pick_results['cf_p']))
+ax2.plot(tp, pick_results['cf_p'])
+ax2.plot(tp, pick_results['cf_s'])
+ax2.set_xlim(0, 7200)
+ax2.grid()
+ax2.set_ylabel('Probability ratio (log10)')
+
+ax1.set_xlabel('Time (second)')
+fig.tight_layout()
 plt.show()
