38a48a8ef016f67c3e381c5edf2c31f9a8a041a5
Lijun Zhu <gatechzhu@gmail.com>
2019-07-08 20:45:30 -0400

    update rbp example

--- examples/rbp.py
+++ examples/rbp.py
@@ -4,64 +4,125 @@ import time
 import matplotlib.pyplot as plt
 import numpy as np
 import torch
-from scipy.ndimage.filters import gaussian_filter1d
-from scipy.special import expit
 from torch import nn
 
 import yews.transforms as tf
+from yews.cpic import pick
 from yews.cpic.utils import sliding_window_view
 from yews.datasets.utils import stream2array
 from yews.models import cpic_v1
+from yews.models import cpic_v2
 
 t0 = time.time()
 waveform = np.load('rbp.npy')
 print(f"Loading data takes {time.time() - t0} seconds.")
-print(waveform.shape)
-t0 = time.time()
-model = cpic_v1(pretrained=False)
-with open('cpic_model.pickle', 'rb') as f:
-    state_dict = pickle.load(f)
-model.load_state_dict(state_dict)
-model.eval()
-print(f"Loading model takes {time.time() - t0} seconds.")
 
 transform = tf.Compose([
     tf.ZeroMean(),
-    tf.SoftClip(1e-3),
+    tf.SoftClip(1e-4),
     tf.ToTensor(),
 ])
 
-t0 = time.time()
-windows = np.squeeze(sliding_window_view(waveform, [3, 2000], [1, 20]))
-windows = torch.stack([transform(window) for window in windows])
-print(f"Breaking into windows takes {time.time() - t0} seconds.")
-print(windows.shape)
+fig, axes = plt.subplots(5, 1, sharex=True, figsize=(12, 8))
+
+
+# original cpic outputs
+model = cpic_v1(pretrained=False)
+with open('cpic_original.pickle', 'rb') as f:
+    state_dict = pickle.load(f)
+model.load_state_dict(state_dict)
+model.eval()
 
 t0 = time.time()
-outputs = []
-batch = 15
-with torch.no_grad():
-    for i in range(0, 1000, batch):
-        outputs.append(model(windows[i:(i+batch)]))
-outputs = np.concatenate(outputs, axis=0)
-print(f"Computing outputs on windows takes {time.time() - t0} seconds.")
-print(outputs.shape)
+pick_results = pick(waveform, 100, 20, model, transform, 0.5, 15)
+print(f"Picking takes {time.time() - t0} seconds.")
+cf_p = pick_results['cf_p']
+cf_s = pick_results['cf_s']
 
-# convert outputs to probabilities
-probs = expit(outputs).T
-probs /= probs.sum(axis=0)
+ax = axes[1]
+td = np.linspace(5, 225, cf_p.shape[0])
+ax.plot(td, cf_p, 'b:', label='P CF')
+ax.plot(td, cf_s, 'g:', label='S CF')
+ax.axvline(x=120, color='b', linestyle='-', label='P catalog')
+ax.axvline(x=134, color='g', linestyle='-', label='S catalog')
+ax.set_xlim([0, 240])
+ax.set_ylabel('CF')
+ax.grid(True)
+ax.legend()
 
-# convert probabilities to cfs
-cf_p = np.log10(probs[1] / (probs[0] + 1e-5))
-cf_s = np.log10(probs[2] / (probs[0] + 1e-5))
-cf_p[probs.argmax(axis=0) != 1] = 0
-cf_s[probs.argmax(axis=0) != 2] = 0
-sigma=3
-cf_p = gaussian_filter1d(cf_p, sigma=sigma)
-cf_s = gaussian_filter1d(cf_s, sigma=sigma)
-print(cf_p.shape)
+# fine-tuned original cpic outputs
+model = cpic_v1(pretrained=False)
+with open('cpic_original_finetuned.pickle', 'rb') as f:
+    state_dict = pickle.load(f)
+model.load_state_dict(state_dict)
+model.eval()
 
-fig, axes = plt.subplots(2, 1, sharex=True, figsize=(12, 6))
+t0 = time.time()
+pick_results = pick(waveform, 100, 20, model, transform, 0.5, 15)
+print(f"Picking takes {time.time() - t0} seconds.")
+cf_p = pick_results['cf_p']
+cf_s = pick_results['cf_s']
+
+ax = axes[2]
+td = np.linspace(5, 225, cf_p.shape[0])
+ax.plot(td, cf_p, 'b:', label='P CF')
+ax.plot(td, cf_s, 'g:', label='S CF')
+ax.axvline(x=120, color='b', linestyle='-', label='P catalog')
+ax.axvline(x=134, color='g', linestyle='-', label='S catalog')
+ax.set_xlim([0, 240])
+ax.set_ylabel('CF')
+ax.grid(True)
+ax.legend()
+# simplified cpic outputs
+model = cpic_v2(pretrained=False)
+with open('cpic_simplified.pickle', 'rb') as f:
+    state_dict = pickle.load(f)
+model.load_state_dict(state_dict)
+model.eval()
+
+t0 = time.time()
+pick_results = pick(waveform, 100, 20, model, transform, 0.5, 15)
+print(f"Picking takes {time.time() - t0} seconds.")
+cf_p = pick_results['cf_p']
+cf_s = pick_results['cf_s']
+
+ax = axes[3]
+td = np.linspace(5, 225, cf_p.shape[0])
+ax.plot(td, cf_p, 'b:', label='P CF')
+ax.plot(td, cf_s, 'g:', label='S CF')
+ax.axvline(x=120, color='b', linestyle='-', label='P catalog')
+ax.axvline(x=134, color='g', linestyle='-', label='S catalog')
+ax.set_xlim([0, 240])
+ax.set_ylabel('CF')
+ax.grid(True)
+ax.legend()
+
+# fine-tuned simplified cpic outputs
+model = cpic_v2(pretrained=False)
+with open('cpic_simplified_finetuned.pickle', 'rb') as f:
+    state_dict = pickle.load(f)
+model.load_state_dict(state_dict)
+model.eval()
+
+t0 = time.time()
+pick_results = pick(waveform, 100, 20, model, transform, 0.5, 15)
+print(f"Picking takes {time.time() - t0} seconds.")
+cf_p = pick_results['cf_p']
+cf_s = pick_results['cf_s']
+
+ax = axes[4]
+td = np.linspace(5, 225, cf_p.shape[0])
+ax.plot(td, cf_p, 'b:', label='P CF')
+ax.plot(td, cf_s, 'g:', label='S CF')
+ax.axvline(x=120, color='b', linestyle='-', label='P catalog')
+ax.axvline(x=134, color='g', linestyle='-', label='S catalog')
+ax.set_xlim([0, 240])
+ax.set_ylabel('CF')
+ax.grid(True)
+ax.legend()
+
+
+ax.set_xlabel('Time (s)')
 # Waveform
 ax = axes[0]
 tw = np.linspace(0, 240, waveform.shape[1])
@@ -77,18 +138,6 @@ ax.set_yticklabels(['BHZ', 'BHN', 'BHE'])
 ax.grid(True)
 ax.set_ylabel('Waveforms')
 ax.legend(loc=1)
-# outputs
-ax = axes[1]
-td = np.linspace(-22, 225, cf_p.shape[0])
-ax.plot(td, cf_p, 'b:', label='P CF')
-ax.plot(td, cf_s, 'g:', label='S CF')
-ax.axvline(x=120, color='b', linestyle='-', label='P catalog')
-ax.axvline(x=134, color='g', linestyle='-', label='S catalog')
-ax.set_xlim([0, 240])
-ax.set_xlabel('Time (s)')
-ax.set_ylabel('CF')
-ax.grid(True)
-ax.legend()
 
 fig.tight_layout()
 plt.show()
