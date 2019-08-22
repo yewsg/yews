561dfead449374ea614c57978ca61a78eea87853
Lijun Zhu <gatechzhu@gmail.com>
2019-08-22 13:26:32 -0400

    training example

---
+++ examples/train_cnn.py
@@ -0,0 +1,150 @@
+import torch
+from torch import nn
+from torch.nn import CrossEntropyLoss
+from torch.utils.data import DataLoader
+from torch.utils.data import random_split
+from torch.utils.data import Subset
+
+from yews import Dataset
+from yews import models
+from yews import transforms
+from yews.train import Trainer
+
+class Cpic40(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+
+        self.layer1 = nn.Sequential(
+            nn.Conv1d(3, 16, kernel_size=5, stride=1, padding=26, bias=False),
+            nn.BatchNorm1d(16),
+            nn.ReLU(),
+            # nn.Sigmoid(),
+            nn.MaxPool1d(2)
+        )
+
+        # 1024 -> 512
+        self.layer2 = nn.Sequential(
+            nn.Conv1d(16, 32, kernel_size=5, stride=1, padding=2, bias=False),
+            nn.BatchNorm1d(32),
+            nn.ReLU(),
+            # nn.Sigmoid(),
+            nn.MaxPool1d(2)
+        )
+
+        # 512 -> 256
+        self.layer3 = nn.Sequential(
+            nn.Conv1d(32, 64, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm1d(64),
+            nn.ReLU(),
+            nn.MaxPool1d(2)
+        )
+
+        # 256 -> 128
+        self.layer4 = nn.Sequential(
+            nn.Conv1d(64, 64, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm1d(64),
+            nn.ReLU(),
+            nn.MaxPool1d(2)
+        )
+
+        # 128 -> 64
+        self.layer5 = nn.Sequential(
+            nn.Conv1d(64, 64, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm1d(64),
+            nn.ReLU(),
+            nn.MaxPool1d(2)
+        )
+
+        # 64 -> 32
+        self.layer6 = nn.Sequential(
+            nn.Conv1d(64, 64, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm1d(64),
+            nn.ReLU(),
+            nn.MaxPool1d(2)
+        )
+
+        # 32 -> 16
+        self.layer7 = nn.Sequential(
+            nn.Conv1d(64, 64, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm1d(64),
+            nn.ReLU(),
+            nn.MaxPool1d(2)
+        )
+
+        # 16 -> 8
+        self.layer8 = nn.Sequential(
+            nn.Conv1d(64, 64, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm1d(64),
+            nn.ReLU(),
+            nn.MaxPool1d(2)
+        )
+
+        # 8 -> 4
+        self.layer9 = nn.Sequential(
+            nn.Conv1d(64, 64, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm1d(64),
+            nn.ReLU(),
+            nn.MaxPool1d(2)
+        )
+
+        self.fc = nn.Linear(64 * 1, 3)
+
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = self.layer5(out)
+        out = self.layer6(out)
+        out = self.layer7(out)
+        out = self.layer8(out)
+        out = self.layer9(out)
+        out = out.view(out.size(0), -1)
+        out = self.fc(out)
+
+        return out
+
+if __name__ == '__main__':
+    # Preprocessing
+    waveform_transform = transforms.Compose([
+        transforms.ZeroMean(),
+        transforms.SoftClip(1e-4),
+        transforms.ToTensor(),
+    ])
+
+    target_transform = transforms.Compose([
+        transforms.Select(0),
+        transforms.ToInt({
+            'N': 0,
+            'P': 1,
+            'S': 2,
+        }),
+    ])
+
+    # Dataset
+    dset = Dataset(path='/data/ok',
+                   sample_transform=waveform_transform,
+                   target_transform=target_transform)
+    train_length = int(len(dset) * 0.8)
+    val_length = len(dset) - train_length
+    train_set, val_set = random_split(dset, [train_length, val_length])
+    # Uncomment for chronological split
+    # train_set = Subset(dset, range(train_length))
+    # val_set = Subset(dset, range(train_length, len(dset)))
+
+    # Dataloaders
+    train_loader = DataLoader(train_set, batch_size=100, shuffle=True, num_workers=4)
+    val_loader = DataLoader(val_set, batch_size=1000, shuffle=False, num_workers=8)
+
+    # CNN model
+    model = Cpic40()
+
+    # Trainer
+    trainer = Trainer(model, CrossEntropyLoss(), lr=0.1)
+
+    # Training process
+    trainer.train(train_loader, val_loader, epochs=10, print_freq=100)
+
+    # Save training results to disk
+    trainer.results(path='wenchuan_results.pth.tar')
