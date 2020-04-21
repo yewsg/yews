db9397d8c679ecd701af7701d0ca008803b50ca5
Lijun Zhu <gatechzhu@gmail.com>
2020-04-21 15:35:31 -0500

    Test pretrained model for CPIC.

--- tests/test_models.py
+++ tests/test_models.py
@@ -2,10 +2,10 @@ import pytest
 import torch
 from yews import models
 
-
+@pytest.mark.internet
 class TestCPICModel:
 
     def test_cpic_model(self):
         dummpy_input = torch.randn(1, 3, 2000)
-        model = models.cpic()
+        model = models.cpic(pretrained=True)
         model(dummpy_input)
