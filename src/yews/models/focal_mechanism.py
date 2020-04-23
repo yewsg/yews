3e9922e5a3281b7d2834b4f93cadbd8ba23cc9d0
zjzzqs <zjzzqs@gmail.com>
2020-04-23 11:14:00 -0700

    add vgg style model for grad-cam, remove the last 2 cnn layers, stop at 4*4

--- src/yews/models/focal_mechanism.py
+++ src/yews/models/focal_mechanism.py
@@ -3,10 +3,10 @@ import torch.nn as nn
 from .utils import load_state_dict_from_url
 
 # import torch
-#try:
-#    from torch.hub import load_state_dict_from_url
-#except ImportError:
-#    from torch.utils.model_zoo import load_url as load_state_dict_from_url
+# try:
+#     from torch.hub import load_state_dict_from_url
+# except ImportError:
+#     from torch.utils.model_zoo import load_url as load_state_dict_from_url
     
 __all__ = ['FmV1', 'fm_v1', 'FmV2', 'fm_v2']
 
@@ -23,16 +23,16 @@ class FmV1(nn.Module):
         # nn.Cov2d(3,16,kernel_size=(3,7),stride=1,padding=....)
         # (3,7) 3>71 7>18000 in the end use (3,3).
 
-        # 71,9000 -> 71,4505
+        # 71,9000 -> 71,4507
         self.layer1 = nn.Sequential(
-            nn.Conv2d(3, 16, kernel_size=(3,13), stride=1, padding=(2,12), bias=False),
+            nn.Conv2d(3, 16, kernel_size=(3,17), stride=1, padding=(2,16), bias=False),
             nn.BatchNorm2d(16),
             nn.ReLU(),
             # nn.Sigmoid(),
             nn.MaxPool2d(kernel_size=3, stride=(1, 2))
         )
 
-        # 71,4505 -> 71,2259
+        # 71,4507 -> 71,2260
         self.layer2 = nn.Sequential(
             nn.Conv2d(16, 32, kernel_size=(3,15), stride=1, padding=(2,14), bias=False),
             nn.BatchNorm2d(32),
@@ -41,7 +41,7 @@ class FmV1(nn.Module):
             nn.MaxPool2d(kernel_size=3, stride=(1, 2))
         )
 
-        # 71,2259 -> 71,1135
+        # 71,2260 -> 71,1135
         self.layer3 = nn.Sequential(
             nn.Conv2d(32, 64, kernel_size=(3,13), stride=1, padding=(2,12), bias=False),
             nn.BatchNorm2d(64),
@@ -168,6 +168,7 @@ def fm_v1(pretrained=False, progress=True, **kwargs):
         model.load_state_dict(state_dict)
     return model
 
+
 class FmV2(nn.Module):
     
     #https://github.com/pytorch/vision/blob/master/torchvision/models/vgg.py
@@ -176,33 +177,33 @@ class FmV2(nn.Module):
         super(FmV2, self).__init__()
         self.features = nn.Sequential(
 
-            # 71,9000 -> 71,4505
+            # 71,9000 -> 71,4507
             
-            nn.Conv2d(3, 16, kernel_size=(3,13), stride=1, padding=(2,12), bias=False),
+            nn.Conv2d(3, 16, kernel_size=(3,17), stride=1, padding=(2,16), bias=False),
             nn.BatchNorm2d(16),
             nn.ReLU(),
 #             nn.Conv2d(16, 16, kernel_size=3, stride=1, padding=1),
 #             nn.BatchNorm2d(16),
 #             nn.ReLU(),
             nn.MaxPool2d(kernel_size=3, stride=(1, 2)),
-            nn.Dropout(0.1),
+#             nn.Dropout(0.1),
             
-            # 71,4505 -> 71,2259
+            # 71,4507 -> 71,2260
 
             nn.Conv2d(16, 32, kernel_size=(3,15), stride=1, padding=(2,14), bias=False),
             nn.BatchNorm2d(32),
             nn.ReLU(),
             # nn.Sigmoid(),
             nn.MaxPool2d(kernel_size=3, stride=(1, 2)),
-            nn.Dropout(0.1),
+#             nn.Dropout(0.1),
 
-            # 71,2259 -> 71,1135
+            # 71,2260 -> 71,1135
 
             nn.Conv2d(32, 64, kernel_size=(3,13), stride=1, padding=(2,12), bias=False),
             nn.BatchNorm2d(64),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=3, stride=(1, 2)),
-            nn.Dropout(0.1),
+#             nn.Dropout(0.1),
 
 
             # 71,1135 -> 71,572
@@ -211,7 +212,7 @@ class FmV2(nn.Module):
             nn.BatchNorm2d(64),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=3, stride=(1, 2)),
-            nn.Dropout(0.1),
+#             nn.Dropout(0.1),
 
 
             # 71,572 -> 71,289
@@ -220,7 +221,7 @@ class FmV2(nn.Module):
             nn.BatchNorm2d(64),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=3, stride=(1, 2)),
-            nn.Dropout(0.1),
+#             nn.Dropout(0.1),
 
 
             # 71,289 -> 71,143
@@ -229,7 +230,7 @@ class FmV2(nn.Module):
             nn.BatchNorm2d(64),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=3, stride=(1, 2)),
-            nn.Dropout(0.1),
+#             nn.Dropout(0.1),
 
 
             # 71,143 -> 71,71
@@ -238,7 +239,7 @@ class FmV2(nn.Module):
             nn.BatchNorm2d(64),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=3, stride=(1, 2)),
-            nn.Dropout(0.1),
+#             nn.Dropout(0.1),
 
             # 71,71 -> 35,35
 
@@ -246,7 +247,7 @@ class FmV2(nn.Module):
             nn.BatchNorm2d(64),
             nn.ReLU(),
             nn.MaxPool2d(2),
-            nn.Dropout(0.1),
+#             nn.Dropout(0.1),
 
 
             # 35,35 -> 17,17
@@ -255,7 +256,7 @@ class FmV2(nn.Module):
             nn.BatchNorm2d(64),
             nn.ReLU(),
             nn.MaxPool2d(2),
-            nn.Dropout(0.1),
+#             nn.Dropout(0.1),
 
         
             # 17,17 -> 8,8
@@ -264,57 +265,47 @@ class FmV2(nn.Module):
             nn.BatchNorm2d(64),
             nn.ReLU(),
             nn.MaxPool2d(2),
-            nn.Dropout(0.1),
-            
+#             nn.Dropout(0.1),
+                        
             # 8,8 -> 4,4
 
             nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1, bias=False),
             nn.BatchNorm2d(64),
             nn.ReLU(),
             nn.MaxPool2d(2),
-            nn.Dropout(0.1),
+#             nn.Dropout(0.1),
+                        
+#             # 4,4 -> 2,2
 
-        
-            # 4,4 -> 2,2
+#             nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1, bias=False),
+#             nn.BatchNorm2d(64),
+#             nn.ReLU(),
+#             nn.MaxPool2d(2),
+# #             nn.Dropout(0.1),
+                        
+#             # 2,2 -> 1,1
 
-            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1, bias=False),
-            nn.BatchNorm2d(64),
-            nn.ReLU(),
-            nn.MaxPool2d(2),
-            nn.Dropout(0.1),
+#             nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1, bias=False),
+#             nn.BatchNorm2d(64),
+#             nn.ReLU(),
+#             nn.MaxPool2d(2),
+# #             nn.Dropout(0.1),
+                        
 
-
-            # 2,2 -> 1,1
-
-            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1, bias=False),
-            nn.BatchNorm2d(64),
-            nn.ReLU(),
-            nn.MaxPool2d(2),
-            nn.Dropout(0.1),
-        
         )
 
-
         self.classifier = nn.Sequential(
-            nn.Linear(64*1*1, 3),
+            nn.Linear(64 * 4 * 4, 3),
         )
             
     def forward(self, x):
         x = self.features(x)
-        x = x.view(x.size(0), -1)
+        x = x.view(x.size(0), -1) 
         x = self.classifier(x)
         return x
 
 def fm_v2(pretrained=False, progress=True, **kwargs):
-    r"""Original CPIC model architecture from the
-    `"Deep learning for ..." <https://arxiv.org/abs/1901.06396>`_ paper. The
-    pretrained model is trained on 60,000 Wenchuan aftershock dataset
-    demonstrated in the paper.
 
-    Args:
-        pretrained (bool): If True, returns a model pre-trained on Wenchuan)
-        progress (bool): If True, displays a progress bar of the download to stderr
-    """
     model = FmV2(**kwargs)
     if pretrained:
         state_dict = load_state_dict_from_url(model_urls['fm_v2'],
@@ -322,11 +313,9 @@ def fm_v2(pretrained=False, progress=True, **kwargs):
         model.load_state_dict(state_dict)
     return model
 
-
-#if __name__ == '__main__':
-#    #model = fm_v1(pretrained=False)
-#    model = fm_v2(pretrained=False)
-#    
-#    x = torch.ones([1, 3, 71, 9000])
-#    out = model(x)
-#    print(out.size())
+# if __name__ == '__main__':
+#     model = fm_v1(pretrained=False)
+    
+#     x = torch.ones([1, 3, 71, 9000])
+#     out = model(x)
+#     print(out.size())
